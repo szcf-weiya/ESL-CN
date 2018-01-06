8da6d5a839d98c453b400fe29f36482d24e2ef02
szcf-weiya <2215235182@qq.com>
2018-01-07 02:02:49 +0800

    update

--- code/ModelSelection/simulation7-7.R
+++ code/ModelSelection/simulation7-7.R
@@ -20,7 +20,7 @@ genY <- function(X, case = 1){
 }
 
 ## global parameters setting
-ntest = 1000
+ntest = 10000
 B = 100 # the number of repetition
 
 ## generate test data
@@ -53,28 +53,37 @@ for (i in 1:B)
     pred = predict(model, X.test)
     # for classification
     pred.cl = sapply(pred, function(x) ifelse(x > 0.5, 1, 0))
-    epe[k] = mean((pred - Y.test)^2)
-    epe.cl[k] = mean(pred.cl!=Y.test)
+    epe[k-4] = mean((pred - Y.test)^2)
+    epe.cl[k-4] = mean(pred.cl!=Y.test)
     # the training error
     yhat = predict(model, X) 
     yhat.cl = sapply(yhat, function(x) ifelse(x > 0.5, 1, 0))
     err = yhat - Y
     err.cl = (yhat.cl != Y)
-    aic[k] = mean(err^2)*(1 + 2/k * (N/(N-N/k)) )
-    bic[k] = mean(err^2)*(1 + log(nrow(X))/k * (N/(N-N/k)) )
-    aic.cl[k] = mean(err.cl)*(1 + 2/k * (N/(N-N/k)) )
-    bic.cl[k] = mean(err.cl)*(1 + log(nrow(X))/k * (N/(N-N/k)) )
+    aic[k-4] = mean(err^2)*(1 + 2/k * (N/(N-N/k)) )
+    bic[k-4] = mean(err^2)*(1 + log(nrow(X))/k * (N/(N-N/k)) )
+    aic.cl[k-4] = mean(err.cl)*(1 + 2/k * (N/(N-N/k)) )
+    bic.cl[k-4] = mean(err.cl)*(1 + log(N)/k * (N/(N-N/k)) )
   }
-  ## k = 1, the training error would be zero
-  aic[1] = Inf 
-  bic[1] = Inf
-  aic.cl[1] = Inf
-  bic.cl[1] = Inf
   err.aic[i] = (epe[which.min(aic)] - min(epe)) / (max(epe) - min(epe))
   err.bic[i] = (epe[which.min(bic)] - min(epe)) / (max(epe) - min(epe))
   err.cl.aic[i] = (epe.cl[which.min(aic.cl)] - min(epe.cl)) / (max(epe.cl) - min(epe.cl))
   err.cl.bic[i] = (epe.cl[which.min(bic.cl)] - min(epe.cl)) / (max(epe.cl) - min(epe.cl))
 }
+## AIC plot
+png("boxplot-AIC-kNN.png")
+df.aic = data.frame(val = c(err.aic, err.cl.aic),
+                    case = factor(c(rep("reg/kNN", B), rep("class/kNN", B)), 
+                                  levels = c("reg/kNN", "class/kNN")) )
+boxplot(val ~ case, data = df.aic, col = "green",
+        main = "AIC", ylab = "Increase over best")
+dev.off()
 
-boxplot(err.aic, err.cl.aic)
-boxplot(err.bic, err.cl.bic)
+## BIC plot
+png("boxplot-BIC-kNN.png")
+df.bic = data.frame(val = c(err.bic, err.cl.bic),
+                    case = factor(c(rep("reg/kNN", B), rep("class/kNN", B)), 
+                                  levels = c("reg/kNN", "class/kNN")) )
+boxplot(val ~ case, data = df.bic, col = "green",
+        main = "BIC", ylab = "Increase over best")
+dev.off()
