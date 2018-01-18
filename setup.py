7aee1006451963ea16ae861ffaebec44f7ca6bb1
szcf-weiya <2215235182@qq.com>
2018-01-19 01:40:20 +0800

    update

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.1'
+
+
+setup(
+    name="mkdocs-yeti",
+    version=VERSION,
+    url='',
+    license='',
+    description='',
+    author='weiya',
+    author_email='szcfweiya@gmail.com',
+    packages=find_packages(),
+    include_package_data=True,
+    entry_points={
+        'mkdocs.themes': [
+            'yeti = yeti',
+        ]
+    },
+    zip_safe=False
+)
