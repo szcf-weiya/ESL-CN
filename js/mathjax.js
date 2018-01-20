d7ae72b5e02be51d421cb9528ff5c58fbe56bb08
szcf-weiya <2215235182@qq.com>
2018-01-20 15:07:53 +0800

    Deployed d6f834f with MkDocs version: 0.17.2

---
+++ js/mathjax.js
@@ -0,0 +1,28 @@
+<!--mathjax-->
+<script data-cfasync="false" type="text/x-mathjax-config">
+  MathJax.Hub.Config({
+  tex2jax: {
+    inlineMath: [['$','$'], ['\\(','\\)']],
+    processEscapes:true
+  },
+  TeX: {
+    Macros: {
+      LOG: "{\\mathrm{log }}",
+      E: "{\\mathrm{E }}",
+      1: "{\\boldsymbol 1}",
+
+      A: "{\\mathbf{A}}",
+      B: "{\\mathbf{B}}",
+      C: "{\\mathbf{C}}",
+      D: "{\\mathbf{D}}",
+      R: "{\\mathbf{R}}",
+      S: "{\\mathbf{S}}",
+      I: "{\\mathbf{I}}",
+      X: "{\\mathbf{S}}"
+    },
+    entensions: ["color.js"]
+  }
+  });
+</script>
+<script data-cfasync="false" type="text/javascript" async src="https://cdn.bootcss.com/mathjax/2.7.2-beta.0/MathJax.js?config=TeX-AMS-MML_HTMLorMML">
+</script>
