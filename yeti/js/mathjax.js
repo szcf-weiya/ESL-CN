bd72250f8b834af6207b01bac0d3ab7d9f5632cc
szcf-weiya <2215235182@qq.com>
2018-01-20 15:43:35 +0800

    update

--- yeti/js/mathjax.js
+++ yeti/js/mathjax.js
@@ -1,28 +1,25 @@
-<!--mathjax-->
-<script data-cfasync="false" type="text/x-mathjax-config">
-  MathJax.Hub.Config({
-  tex2jax: {
-    inlineMath: [['$','$'], ['\\(','\\)']],
-    processEscapes:true
+MathJax.Hub.Config({
+tex2jax: {
+  inlineMath: [['$','$'], ['\\(','\\)']],
+  processEscapes:true
+},
+TeX: {
+  Macros: {
+    LOG: "{\\mathrm{log }}",
+    E: "{\\mathrm{E }}",
+    1: "{\\boldsymbol 1}",
+
+    A: "{\\mathbf{A}}",
+    B: "{\\mathbf{B}}",
+    C: "{\\mathbf{C}}",
+    D: "{\\mathbf{D}}",
+    R: "{\\mathbf{R}}",
+    S: "{\\mathbf{S}}",
+    I: "{\\mathbf{I}}",
+    X: "{\\mathbf{S}}",
+
+    Cov: "{\\mathrm{Cov }}"
   },
-  TeX: {
-    Macros: {
-      LOG: "{\\mathrm{log }}",
-      E: "{\\mathrm{E }}",
-      1: "{\\boldsymbol 1}",
-
-      A: "{\\mathbf{A}}",
-      B: "{\\mathbf{B}}",
-      C: "{\\mathbf{C}}",
-      D: "{\\mathbf{D}}",
-      R: "{\\mathbf{R}}",
-      S: "{\\mathbf{S}}",
-      I: "{\\mathbf{I}}",
-      X: "{\\mathbf{S}}",
-
-      Cov: "{\\mathrm{Cov }}"
-    },
-    entensions: ["color.js"]
-  }
-  });
-</script>
+  entensions: ["color.js"]
+}
+});
