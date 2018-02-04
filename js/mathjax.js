bf1f51a9102d9a7fed1844e003aa9d06653fe882
szcf-weiya <2215235182@qq.com>
2018-02-04 10:06:21 +0800

    Deployed 47a0b31f4 with MkDocs version: 0.17.2

--- js/mathjax.js
+++ js/mathjax.js
@@ -1,26 +1,26 @@
-MathJax.Hub.Config({
-tex2jax: {
-  inlineMath: [['$','$'], ['\\(','\\)']],
-  processEscapes:true
-},
-TeX: {
-  Macros: {
-    LOG: "{\\mathrm{log }}",
-    E: "{\\mathrm{E }}",
-    1: "{\\boldsymbol 1}",
-
-    A: "{\\mathbf{A}}",
-    B: "{\\mathbf{B}}",
-    C: "{\\mathbf{C}}",
-    D: "{\\mathbf{D}}",
-    R: "{\\mathbf{R}}",
-    S: "{\\mathbf{S}}",
-    I: "{\\mathbf{I}}",
-    X: "{\\mathbf{S}}",
-
-    Cov: "{\\mathrm{Cov }}",
-    Var: "{\\mathrm{Var }}"
+window.MathJax = {
+  tex2jax: {
+    inlineMath: [['$','$'], ['\\(','\\)']],
+    processEscapes:true
   },
-  entensions: ["color.js"]
-}
-});
+  TeX: {
+    Macros: {
+      LOG: "{\\mathrm{log }}",
+      E: "{\\mathrm{E }}",
+      1: "{\\boldsymbol 1}",
+
+      A: "{\\mathbf{A}}",
+      B: "{\\mathbf{B}}",
+      C: "{\\mathbf{C}}",
+      D: "{\\mathbf{D}}",
+      R: "{\\mathbf{R}}",
+      S: "{\\mathbf{S}}",
+      I: "{\\mathbf{I}}",
+      X: "{\\mathbf{S}}",
+
+      Cov: "{\\mathrm{Cov }}",
+      Var: "{\\mathrm{Var }}"
+    },
+    entensions: ["color.js"]
+  }
+};
