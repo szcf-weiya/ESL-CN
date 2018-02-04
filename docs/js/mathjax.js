a68b4e0aa411a8575e2c1e3394664d13b201f798
szcf-weiya <2215235182@qq.com>
2018-02-04 20:06:39 +0800

    update

---
+++ docs/js/mathjax.js
@@ -0,0 +1,40 @@
+window.MathJax = {
+  tex2jax: {
+    inlineMath: [['$','$'], ['\\(','\\)']],
+    processEscapes:true
+  },
+  TeX: {
+    Macros: {
+      A: "{\\mathbf{A}}",
+      B: "{\\mathbf{B}}",
+      C: "{\\mathbf{C}}",
+      D: "{\\mathbf{D}}",
+      R: "{\\mathbf{R}}",
+      IR: "{\\mathrm{I\!R}}",
+      S: "{\\mathbf{S}}",
+      I: "{\\mathbf{I}}",
+      J: "{\\mathbf{J}}",
+      X: "{\\mathbf{X}}",
+      Y: "{\\mathbf{Y}}",
+      U: "{\\mathbf{U}}",
+      V: "{\\mathbf{V}}",
+      W: "{\\mathbf{W}}",
+
+      LOG: "{\\mathrm{log}\\;}",
+      E: "{\\mathrm{E}\\;}",
+      1: "{\\boldsymbol 1}",
+      Cov: "{\\mathrm{Cov}\\;}",
+      Var: "{\\mathrm{Var}\\;}",
+      det: "{\\mathrm{det}\\;}",
+      cosh: "{\\mathrm{cosh}\\;}",
+      arg: "{\\mathrm{arg}\\;}",
+      max: "{\\mathrm{max}\\;}",
+      min: "{\\mathrm{min}\\;}",
+
+      sign: "{\\mathrm{sign}}",
+      df: "{\\mathrm{df}}",
+      tr: "{\\mathrm{tr}}"
+    },
+    entensions: ["color.js"]
+  }
+};
