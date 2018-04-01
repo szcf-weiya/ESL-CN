ce7202928da214ded5e9bdd64e0caf3cf32f7d65
szcf-weiya <2215235182@qq.com>
2018-04-01 12:40:22 +0800

    Deployed ed08eb6 with MkDocs version: 0.17.2

---
+++ notes/spline/study_wavelet.R
@@ -0,0 +1,62 @@
+library(wavethresh)
+
+## Example 2.3
+y = c(1, 1, 7, 9, 2, 8, 8, 6)
+ywd = wd(y, filter.number = 1, family = "DaubExPhase")
+# obtain the finest-level coefficients
+accessD(ywd, level=2)
+
+## 2.1.3 Matrix Representation
+W1 = t(GenW(filter.number = 1, family = 'DaubExPhase'))
+W1 %*% t(W1)
+
+## #################################################
+## 2.2.7 discrete Haar wavelet transform examples
+## #################################################
+
+## fig. 2.6
+yy = DJ.EX()$blocks
+yywd = wd(yy, filter.number = 1, family = "DaubExPhase")
+x = 1:1024
+oldpar <- par(mfrow=c(2,2))
+plot(x, yy, type = "l", xlab = "x", ylab = "Doppler")
+plot(x, yy, type = "l", xlab = "x", ylab = "Doppler")
+plot(yywd, main = "")
+plot(yywd, scaling = "by.level", main = "")
+par(oldpar)
+
+## fig. 2.7
+yy = DJ.EX()$doppler
+yywd = wd(yy, filter.number = 1, family = "DaubExPhase")
+x = 1:1024
+oldpar <- par(mfrow=c(2,2))
+plot(x, yy, type = "l", xlab = "x", ylab = "Doppler")
+plot(x, yy, type = "l", xlab = "x", ylab = "Doppler")
+plot(yywd, main = "")
+plot(yywd, scaling = "by.level", main = "")
+par(oldpar)
+
+## ##################################################
+## 2.5.1 Daubechies' compactly supported wavelets
+## ##################################################
+
+filter.select(filter.number = 1, family = "DaubExPhase") # from 1(Haar) to 10
+filter.select(filter.number = 4, family = "DaubLeAsymm") # from 4 to 10
+
+## fig. 2.8
+oldpar = par(mfrow = c(2, 1))
+draw.default(filter.number = 4, family = "DaubExPhase", enhance = FALSE,  main = "a.")
+draw.default(filter.number = 4, family = "DaubExPhase", enhance = FALSE,  scaling.function = TRUE, main = "a.")
+par(oldpar)
+
+## fig. 2.9
+oldpar = par(mfrow = c(2, 1))
+draw.default(filter.number = 10, family = "DaubExPhase", enhance = FALSE,  main = "a.")
+draw.default(filter.number = 10, family = "DaubExPhase", enhance = FALSE,  scaling.function = TRUE, main = "a.")
+par(oldpar)
+
+W2 = t(GenW(n=8, filter.number = 3, family = "DaubExPhase"))
+W2
+
+## Example 2.4 (continue Example 2.3)
+yinv = wr(ywd)
