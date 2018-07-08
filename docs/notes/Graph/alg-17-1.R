5eb64ed65d2029dbe5c791fd9f9c0c6c80c4f9ed
szcf-weiya <2215235182@qq.com>
2018-07-08 15:01:46 +0800

    use adjacency matrix

--- docs/notes/Graph/alg-17-1.R
+++ docs/notes/Graph/alg-17-1.R
@@ -19,7 +19,7 @@ estUGGM <- function(S)
     i = 0
     converge = FALSE
     finalcycle = FALSE
-    Omega = matrix(ncol=4,nrow=4)
+    Omega = matrix(ncol=p,nrow=p)
     while(TRUE)
     {
         for (j in 1:p)
@@ -84,21 +84,88 @@ estUGGM <- function(S)
                 Omega[j, -j] = theta12
                 Omega[-j, j] = theta12
             }
-            if (finalcycle && j == 4)
+            if (finalcycle && j == p)
             {
                 break
             }
         }
-        if (finalcycle && j == 4)
+        if (finalcycle && j == p)
             break
     }
     ## matrix sigma
     print(W)
     print(Omega)
 }
+## use adjacency matrix as graph structure
+estUGGM2 <- function(S, adj)
+{
+  ## initialize
+  W = S
+  p = nrow(S)
+  ## repeat until convergence
+  i = 0
+  converge = FALSE
+  finalcycle = FALSE
+  Omega = matrix(ncol=p,nrow=p)
+  while(TRUE)
+  {
+    for (j in 1:p)
+    {
+      if (j == 1 && converge)
+        finalcycle = TRUE
+      beta.hat = matrix(rep(0,p-1),ncol=1)
+      beta.hat.add.j = matrix(rep(0,p),ncol=1)
+      idx = c(1:p, j)[-j]
+      W.reorder = W[idx, idx]
+      S.reorder = S[idx, idx]
+      W.11 = W[-j, -j]
+      ## W11.star, s12.star base on the known graph structure
+      ## use adjacency matrix
+      edge.idx = which(adj[j,] != 0)
+      W.11.star = W[edge.idx, edge.idx]
+      s.12.star = S[j, edge.idx]
+      ## solve reduced system of equations
+      beta.star = solve(W.11.star, s.12.star)
+      ## update
+      beta.hat.add.j[edge.idx] = beta.star
+      beta.hat = beta.hat.add.j[-j]
+      W.reorder[p, -p] = W.11 %*% beta.hat
+      W.reorder[-p, p] = W.11 %*% beta.hat
+      W.backup = W
+      W = W.reorder[order(idx), order(idx)]
+      err = norm(W.backup-W)
+      cat("j = ", j," err = ", err, "\n")
+      if (err < 1e-12)
+        converge = 1
+      ## final cycle
+      if (finalcycle)
+      {
+        theta22 = 1/(S[j, j] - sum(W.reorder[p,-p]*beta.hat))
+        theta12 = -1.0 * beta.hat * theta22
+        Omega[j, j] = theta22
+        Omega[j, -j] = theta12
+        Omega[-j, j] = theta12
+      }
+      if (finalcycle && j == p)
+      {
+        break
+      }
+    }
+    if (finalcycle && j == p)
+      break
+  }
+  ## matrix sigma
+  print(W)
+  print(Omega)
+}
 ## example
 S = matrix(c(10, 1, 5, 4,
              1, 10, 2, 6,
              5, 2, 10, 3,
              4, 6, 3, 10), ncol = 4)
+adj = matrix(c(0, 1, 0, 1,
+               1, 0, 1, 0,
+               0, 1, 0, 1,
+               1, 0, 1, 0), ncol = 4)
 estUGGM(S)
+estUGGM2(S, adj)
