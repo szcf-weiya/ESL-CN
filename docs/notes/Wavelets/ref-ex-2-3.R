37802d5d62c7fd27df25f762be31bdd8fe052a03
szcf-weiya <2215235182@qq.com>
2018-07-15 22:58:05 +0800

    notes for wavelets

---
+++ docs/notes/Wavelets/ref-ex-2-3.R
@@ -0,0 +1,11 @@
+library(wavethresh)
+# create the vector that contains our input to the transform
+y = c(1, 1, 7, 9, 2, 8, 8, 6)
+# Haar wavelet
+ywd = wd(y, filter.number = 1, family = "DaubExPhase")
+# filter
+ywd$filter
+# extract the finest-level coefficients
+accessD(ywd, level = 2)
+# plot for wavelet decomposition coefficients
+plot(ywd)
