f0d3c09098b6a71098eea65790c1b1b4cd37b9e9
szcf-weiya <2215235182@qq.com>
2018-07-17 16:32:59 +0800

    update wavelet notes

--- docs/notes/Wavelets/ref-note.R
+++ docs/notes/Wavelets/ref-note.R
@@ -26,3 +26,43 @@ par(oldpar)
 
 ## looking at the first 15 coefficients at level eight
 accessD(wd(DJ.EX()$blocks), level = 8)[1:15]
+
+## ##################################
+## universal thresholding
+## ##################################
+v = DJ.EX()
+# define X coordinates too
+x = (1:1024)/1024
+plot(x, v$bumps, type = "l", ylab="Bumps")
+# specify a SNR of 2
+ssig = sd(v$bumps)
+SNR = 2 # fix SNR
+sigma = ssig/SNR
+e = rnorm(1024, 0, sigma)
+y = v$bumps + e
+plot(x, y, type = "l", ylab = "Noisy bumps")
+#
+# plot wd of bumps
+# 
+xlv = seq(from = 0, to = 1.0, by = 0.2)
+bumpswd = wd(v$bumps)
+plot(bumpswd, main = "", sub = "",
+     xlabvals = xlv*512, xlabchars=as.character(xlv),
+     xlab="x")
+#
+# plot wd of noisy bumps for comparison
+# 
+ywd = wd(y)
+plot(ywd, main = "", sub = "",
+     xlabvals = xlv*512, xlabchars=as.character(xlv),
+     xlab = "x")
+
+## extract the finest-level coefficients
+FineCoefs = accessD(ywd, level = nlevelsWT(ywd)-1)
+sigma = mad(FineCoefs)
+utDJ = sigma*sqrt(2*log(1024))
+ywdT = threshold(ywd, policy = "manual", value = utDJ)
+# wavelet reconstruction
+ywr = wr(ywdT)
+plot(x, ywr, type = "l")
+lines(x, v$bumps, lty = 2)
