bed11f292996f42e91753313202442046aa09948
szcf-weiya <2215235182@qq.com>
2019-02-18 15:40:26 +0800

    resolve #178

---
+++ docs/03-Linear-Methods-for-Regression/assets/script.jl
@@ -0,0 +1,11 @@
+using Plots
+t = collect(-4:0.2:4)
+function huber(t; λ = 1)
+    flag = (abs.(t) .> λ)
+    y1 = t.^2 / 2 .* flag .+ (λ * abs.(t) .- λ^2 / 2) .* (1 .- flag)
+    y2 = λ * abs.(t) .- λ^2 / 2
+    return y1, y2
+end
+y1, y2 = huber(t)
+plot(t, y1, label = "Huber", legend = :top)
+plot!(t, y2, label = "Linear")
