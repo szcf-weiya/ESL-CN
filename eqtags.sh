91a99508d0f2da99bf4562bb9b79b45278ae912e
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-06-27 03:08:37 +0000

    Deploy szcf-weiya/ESL-CN to github.com/szcf-weiya/ESL-CN.git:gh-pages

--- eqtags.sh
+++ eqtags.sh
@@ -3,3 +3,9 @@ sed -i "s/qquad[ ]*(\([0-9]\{1,2\}\.[0-9]\{1,2\}\))/tag{\1}/g"
 
 # why three backslash: https://stackoverflow.com/questions/2369314/why-does-sed-require-3-backslashes-for-a-regular-backslash
 sed -i "s/tag{\([0-9]\{1,2\}\.[0-9]\{1,2\}\)}/tag{\1}\\\label{\1}/g"
+
+sed -i "s/。/．/g"
+
+find . -regextype sed -regex "./[0-9]\{2\}-.*.md"
+
+find . -regextype sed -regex "./[0-9]\{2\}-.*.md" | xargs sed -i "s/。/．/g"
