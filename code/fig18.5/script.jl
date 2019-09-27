f83b9211e1d9b73493563f8488def714c6db3dc1
szcf-weiya <2215235182@qq.com>
2019-09-27 12:20:07 +0800

    julia version for fig18.5 #207

--- code/fig18.5/script.jl
+++ code/fig18.5/script.jl
@@ -10,6 +10,11 @@ train_X = raw_X[:, 3:2:end]
 train_X = train_X[:, 1:end-1]
 # separate the id and the data
 id = Int.(train_X[1, :])
-train_X = Float64.(train_X[2:end, :])
+train_X = Array{Float64, 2}(train_X[2:end, :]')
 # get from `table_ALL_AML_samples.rtf`
-train_y = vcat(fill(1, 27, 1), fill(2, 38-27, 1))
+train_y = hcat(vcat(fill(1, 27, 1), fill(0, 38-27, 1)),
+               vcat(fill(0, 27, 1), fill(1, 38-27, 1)))
+
+grid = collect(range(exp(-8), exp(-1), length=100))
+lasso_path = glmnet(train_X, train_y, Binomial(), lambda=grid)
+elnet_path = glmnet(train_X, train_y, Binomial(), lambda=grid, alpha=0.8)
