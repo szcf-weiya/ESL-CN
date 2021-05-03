7fa6dfe628c98576f9d5c26a36ab8ac9971e7a24
szcf-weiya <2215235182@qq.com>
2021-05-03 20:07:16 +0800

    fix http://disq.us/p/2gseqod

--- docs/03-Linear-Methods-for-Regression/assets/script.jl
+++ docs/03-Linear-Methods-for-Regression/assets/script.jl
@@ -1,7 +1,7 @@
 using Plots
 t = collect(-4:0.2:4)
 function huber(t; λ = 1)
-    flag = (abs.(t) .> λ)
+    flag = (abs.(t) .< λ)
     y1 = t.^2 / 2 .* flag .+ (λ * abs.(t) .- λ^2 / 2) .* (1 .- flag)
     y2 = λ * abs.(t) .- λ^2 / 2
     return y1, y2
